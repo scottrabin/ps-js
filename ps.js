e293fc8ea1d6c48e516245ae84c0edf18f1979dd
Scott Rabin <scottrabin@gmail.com>
2012-09-02 16:33:30 -0700

    Added `on`, `off`, and `trigger`

--- ps.js
+++ ps.js
@@ -1,9 +1,9 @@
 (function(root, factory) {
 	if (typeof define === 'function' && define.amd) {
 		// AMD; don't export to window globals
-		define(factory);
+		define(['is'], factory);
 	} else {
-		root.ps = factory();
+		root.ps = factory(root.is);
 	}
 }(this, function() {
 
@@ -11,6 +11,10 @@
 	 * Basic observable object type
 	 */
 	function Observable(val) {
+		// create an empty hash for function listeners
+		this._listeners = {};
+
+		// set the initial value
 		this.set(val);
 	}
 	/**
@@ -26,6 +30,61 @@
 	Observable.prototype.get = function() {
 		return this._value;
 	};
+
+	/**
+	 * Bind a callback to a given event
+	 */
+	Observable.prototype.on = function(event, callback) {
+		// verify the listener array exists
+		var listeners = (is.array(this._listeners[event]) ?
+						 this._listeners[event] :
+						 this._listeners[event] = []
+						);
+
+		// add the callback to the list
+		listeners.push(callback);
+
+		// chain
+		return this;
+	};
+
+	/**
+	 * Remove a callback from a given event
+	 */
+	Observable.prototype.off = function(event, callback) {
+		// if callback is a function, then remove it from the callback list
+		if ( is.fn(callback) ) {
+			for (var i = 0 ; i < this._listeners[event].length ; i++) {
+				if (this._listeners[event][i] === callback) {
+					this._listeners[event].splice(i--, 1);
+				}
+			}
+
+			// if there are no more items, wipe the listener array
+			if (this._listeners[event].length === 0) {
+				callback = null;
+			}
+		}
+
+		if ( !callback ) {
+			delete this._listeners[event];
+		}
+	};
+
+	/**
+	 * Trigger an event on the observable object
+	 */
+	Observable.prototype.trigger = function(event) {
+		// verify there are listeners for this event
+		if ( this._listeners[event] ) {
+			var args = Array.prototype.slice.call( arguments, 1 ),
+				i;
+			for (i = 0, l = this._listeners[event].length ; i < l ; i++) {
+				this._listeners[event][i].apply(this, args);
+			}
+		}
+	};
+		
 
 	return {
 		create: function(v) {
