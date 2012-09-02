a9a86e9da003855437c003cb6f8e3aa77b5c1398
Scott Rabin <scottrabin@gmail.com>
2012-09-02 15:39:42 -0700

    Basic get/set functionality

--- ps.js
+++ ps.js
@@ -7,4 +7,29 @@
 	}
 }(this, function() {
 
+	/**
+	 * Basic observable object type
+	 */
+	function Observable(val) {
+		this.set(val);
+	}
+	/**
+	 * Basic observable set - sets the current value of the observable
+	 */
+	Observable.prototype.set = function(v) {
+		this._value = v;
+		return this;
+	};
+	/**
+	 * Basic observable get - gets the current value of the observable
+	 */
+	Observable.prototype.get = function() {
+		return this._value;
+	};
+
+	return {
+		create: function(v) {
+			return new Observable(v);
+		}
+	};
 }));
